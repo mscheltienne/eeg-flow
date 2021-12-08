3e5765d4a4b2c76b5eb3b74876c477f97949c822
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-12-08 22:25:58 +0100

    fix __init__ in audio

--- eeg_flow/audio/__init__.py
+++ eeg_flow/audio/__init__.py
@@ -1,8 +1,6 @@
-"""Module for auditory stimulus."""
+"""Module for auditory stimulus using sounddevice."""
 
 from .assr import ASSR  # noqa: F401
 from .tone import Tone  # noqa: F401
 from .sound import Sound  # noqa: F401
 from .whitenoise import WhiteNoise  # noqa: F401
-from .mml import gui_mimimum_masking_level  # noqa: F401
-from .stimulus_selection import gui_stimulus_selection  # noqa: F401
