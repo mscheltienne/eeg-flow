ae1516ebe8d4ef934f07333ce66638f6c1a8ce74
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-03-07 20:39:45 +0100

    clean-up trial script

--- scripts/trial.py
+++ scripts/trial.py
@@ -1,37 +1,43 @@
-from bsl.utils import Timer
-from psychopy import parallel, core
-from eeg_flow.audio import Sound
+from bsl.triggers import ParallelPortTrigger
+from psychopy.clock import wait
+from psychopy.sound.backend_ptb import SoundPTB as Sound
+import psychtoolbox as ptb
 
 
+# Read trial list file
 filename_trialList = "trialList/1000-trialListA.txt"
-
-# read trial list file
 trials = [];
 with open(filename_trialList) as f:
     for line in f:
        trials.append(line.split()[1])
 
-sound_standard = Sound('snd/low_tone-48000.wav')
-sound_target = Sound('snd/high_tone-48000.wav')
+# Create sounds
+sound_standard = Sound('snd/low_tone-48000.wav', blockSize=32,
+                       preBuffer=-1, hamming=True)
+sound_target = Sound('snd/high_tone-48000.wav', blockSize=32,
+                     preBuffer=-1, hamming=True)
 sounds = dict(standard=sound_standard, target=sound_target)
 trigger_values = dict(standard=1, target=2)
 
 for trial in trials:
     if trial in ('standard', 'target'):
         continue
-    sounds[trial] = Sound(f'snd/{trial}-48000.wav')
+    sounds[trial] = Sound(f'snd/{trial}-48000.wav', blockSize=32,
+                          preBuffer=-1, hamming=True)
     trigger_values[trial] = 3
 
-port = parallel.ParallelPort(address='/dev/parport0')
-port.setData(0)
+# Create trigger
+trigger = ParallelPortTrigger('/dev/parport0')
 
+# Inter-trial delay
+delay = 1  # seconds
+scheduling_delay = 0.2  # seconds
+
+# Main loop
 input('Press any key to start..')
-events = [(k+1, trial) for k, trial in enumerate(trials)]
-timer = Timer()
-while True:
-    if timer.sec() >= events[0][0]:
-        ev, trial = events.pop(0)
-        sounds[trial].play()
-        port.setData(trigger_values[trial])
-        core.wait(0.01)
-        port.setData(0)
+
+for trial in trials:
+    sounds[trial].play(when=ptb.GetSecs() + scheduling_delay)
+    wait(scheduling_delay, hogCPUperiod=scheduling_delay + 1)
+    trigger.signal(trigger_values[trial])
+    wait(delay - scheduling_delay, hogCPUperiod=delay)
