a97cc35245253d7203e79c33b1126e1751c56b45
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-06-10 14:01:27 +0200

    clean-up scripts

--- scripts/analysis_with_low_fq.py
+++ scripts/analysis_with_low_fq.py
@@ -1,29 +1,26 @@
-from autoreject import get_rejection_threshold
 import mne
+from autoreject import get_rejection_threshold
 
-from eeg_flow.io import load_xdf, find_streams, create_raw
 from eeg_flow.bad_channels import PREP_bads_suggestion
-
+from eeg_flow.io import create_raw, find_streams, load_xdf
 
 #%% Load from .xdf
-fname = '.xdf'
+fname = ".xdf"
 
 streams = load_xdf(fname)
-eeg_stream = find_streams(streams, 'eego')[0][1]
+eeg_stream = find_streams(streams, "eego")[0][1]
 raw = create_raw(eeg_stream)
 
 #%% Load from .fif
-fname = '-raw.fif'
+fname = "-raw.fif"
 raw = mne.io.read_raw_fif(fname, preload=True)
 
 #%% Set AUX channels' names and types
 mne.rename_channels(
-    raw.info, {'AUX7': 'ECG',
-               'AUX8': 'hEOG',
-               'EOG': 'vEOG',
-               'AUX4': 'EDA'})
+    raw.info, {"AUX7": "ECG", "AUX8": "hEOG", "EOG": "vEOG", "AUX4": "EDA"}
+)
 # AUX5 to be defined
-raw.set_channel_types(mapping={'ECG': 'ecg', 'vEOG': 'eog', 'hEOG': 'eog'})
+raw.set_channel_types(mapping={"ECG": "ecg", "vEOG": "eog", "hEOG": "eog"})
 
 #%% Preprocess
 raw.pick_types(stim=True, eeg=True, eog=True, ecg=True)
@@ -31,40 +28,43 @@ raw_ica_fit = raw.copy()
 # bandpass filter
 raw.filter(
     l_freq=0.1,
-    h_freq=40.,
-    picks=['eeg', 'eog', 'ecg'],
+    h_freq=40.0,
+    picks=["eeg", "eog", "ecg"],
     method="fir",
     phase="zero-double",
     fir_window="hamming",
     fir_design="firwin",
-    pad="edge")
+    pad="edge",
+)
 
 raw_ica_fit.filter(
-    l_freq=1.,
-    h_freq=40.,
-    picks=['eeg', 'eog', 'ecg'],
+    l_freq=1.0,
+    h_freq=40.0,
+    picks=["eeg", "eog", "ecg"],
     method="fir",
     phase="zero-double",
     fir_window="hamming",
     fir_design="firwin",
-    pad="edge")
+    pad="edge",
+)
 
 #%% Search for bads
 bads = PREP_bads_suggestion(raw)  # operates on a copy
-raw.info['bads'] = bads
-raw_ica_fit.info['bads'] = bads
+raw.info["bads"] = bads
+raw_ica_fit.info["bads"] = bads
 
 #%% Add ref channel and montage after adding ref channel
-raw.add_reference_channels(ref_channels='CPz')
-raw_ica_fit.add_reference_channels(ref_channels='CPz')
-raw.set_montage('standard_1020')
-raw_ica_fit.set_montage('standard_1020')
+raw.add_reference_channels(ref_channels="CPz")
+raw_ica_fit.add_reference_channels(ref_channels="CPz")
+raw.set_montage("standard_1020")
+raw_ica_fit.set_montage("standard_1020")
 
 #%% ICA
-ica = mne.preprocessing.ICA(method='picard', max_iter='auto',
-                            n_components=0.99)
+ica = mne.preprocessing.ICA(
+    method="picard", max_iter="auto", n_components=0.99
+)
 # Retrieve EEG channels without CPz and bads
-picks = mne.pick_types(raw.info, eeg=True, exclude=['CPz'] + raw.info['bads'])
+picks = mne.pick_types(raw.info, eeg=True, exclude=["CPz"] + raw.info["bads"])
 ica.fit(raw_ica_fit, picks=picks)
 
 #%% Plot
@@ -75,19 +75,28 @@ ica.plot_sources(raw_ica_fit)
 ica.apply(raw)
 
 #%% Interpolations
-raw.interpolate_bads(reset_bads=True, mode='accurate')
+raw.interpolate_bads(reset_bads=True, mode="accurate")
 
 #%% Reference
-raw.set_eeg_reference(ref_channels=['M1', 'M2'], projection=False,
-                      ch_type='eeg')
-raw.drop_channels(['M1', 'M2'])
+raw.set_eeg_reference(
+    ref_channels=["M1", "M2"], projection=False, ch_type="eeg"
+)
+raw.drop_channels(["M1", "M2"])
 
 #%% Epochs
-events = mne.find_events(raw, stim_channel='TRIGGER')
+events = mne.find_events(raw, stim_channel="TRIGGER")
 events_id = dict(standard=1, target=2, novel=3)
-epochs = mne.Epochs(raw, events, events_id, tmin=-0.2, tmax=0.5,
-                    baseline=(None, 0), reject=None, picks='eeg',
-                    preload=True)
+epochs = mne.Epochs(
+    raw,
+    events,
+    events_id,
+    tmin=-0.2,
+    tmax=0.5,
+    baseline=(None, 0),
+    reject=None,
+    picks="eeg",
+    preload=True,
+)
 reject = get_rejection_threshold(epochs, decim=1)
 epochs.drop_bad(reject=reject)
 
@@ -95,11 +104,11 @@ epochs.drop_bad(reject=reject)
 epochs.plot_drop_log()
 
 #%% Evoked
-evoked_standard = epochs['standard'].average()
-evoked_target = epochs['target'].average()
-evoked_novel = epochs['novel'].average()
+evoked_standard = epochs["standard"].average()
+evoked_target = epochs["target"].average()
+evoked_novel = epochs["novel"].average()
 
 #%% Plot
-evoked_standard.plot_joint(picks='eeg', title='Standard')
-evoked_target.plot_joint(picks='eeg', title='Target')
-evoked_novel.plot_joint(picks='eeg', title='Novel')
+evoked_standard.plot_joint(picks="eeg", title="Standard")
+evoked_target.plot_joint(picks="eeg", title="Target")
+evoked_novel.plot_joint(picks="eeg", title="Novel")
--- scripts/trial.py
+++ scripts/trial.py
@@ -1,40 +1,42 @@
+import psychtoolbox as ptb
 from bsl.triggers import ParallelPortTrigger
 from psychopy.clock import wait
 from psychopy.sound.backend_ptb import SoundPTB as Sound
-import psychtoolbox as ptb
-
 
 # Read trial list file
 filename_trialList = "trialList/1000-trialListA.txt"
-trials = [];
+trials = []
 with open(filename_trialList) as f:
     for line in f:
-       trials.append(line.split()[1])
+        trials.append(line.split()[1])
 
 # Create sounds
-sound_standard = Sound('snd/low_tone-48000.wav', blockSize=32,
-                       preBuffer=-1, hamming=True)
-sound_target = Sound('snd/high_tone-48000.wav', blockSize=32,
-                     preBuffer=-1, hamming=True)
+sound_standard = Sound(
+    "snd/low_tone-48000.wav", blockSize=32, preBuffer=-1, hamming=True
+)
+sound_target = Sound(
+    "snd/high_tone-48000.wav", blockSize=32, preBuffer=-1, hamming=True
+)
 sounds = dict(standard=sound_standard, target=sound_target)
 trigger_values = dict(standard=1, target=2)
 
 for trial in trials:
-    if trial in ('standard', 'target'):
+    if trial in ("standard", "target"):
         continue
-    sounds[trial] = Sound(f'snd/{trial}-48000.wav', blockSize=32,
-                          preBuffer=-1, hamming=True)
+    sounds[trial] = Sound(
+        f"snd/{trial}-48000.wav", blockSize=32, preBuffer=-1, hamming=True
+    )
     trigger_values[trial] = 3
 
 # Create trigger
-trigger = ParallelPortTrigger('/dev/parport0')
+trigger = ParallelPortTrigger("/dev/parport0")
 
 # Inter-trial delay
 delay = 1  # seconds
 scheduling_delay = 0.2  # seconds
 
 # Main loop
-input('Press any key to start..')
+input("Press any key to start..")
 
 for trial in trials:
     sounds[trial].play(when=ptb.GetSecs() + scheduling_delay)
