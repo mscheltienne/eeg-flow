ec4f99d267f203ef6bbc7dca10e3c7b1746fa996
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-01-04 11:32:43 +0100

    better tests

--- eeg_flow/config/config.py
+++ eeg_flow/config/config.py
@@ -1,18 +1,26 @@
 from pathlib import Path
+from typing import Union
 
 from bsl.triggers import TriggerDef
 
 
-def load_triggers():
+def load_triggers(
+    fname: Union[str, Path] = Path(__file__).parent / "triggers.ini"
+) -> TriggerDef:
     """Load triggers from triggers.ini into a TriggerDef instance.
+
+    Parameters
+    ----------
+    fname : str | Path
+        Path to the configuration file.
+        Default to 'eeg_flow/config/triggers.ini'.
 
     Returns
     -------
     tdef : TriggerDef
         Trigger definitiopn containing: standard, target, novel.
     """
-    directory = Path(__file__).parent
-    tdef = TriggerDef(directory / "triggers.ini")
+    tdef = TriggerDef(fname)
 
     keys = (
         "standard",
--- eeg_flow/viz/tests/test_bridged.py
+++ eeg_flow/viz/tests/test_bridged.py
@@ -16,14 +16,14 @@ raw = read_raw_fif(
 def test_plot_bridged_electrodes():
     """Test plotting from a raw object."""
     f, ax = plot_bridged_electrodes(raw)
-    plt.close(f)
 
     with pytest.raises(TypeError, match="must be an instance of BaseRaw"):
         plot_bridged_electrodes(101)
-
     raw_ = raw.copy().filter(1, None)
     with pytest.raises(RuntimeError, match="should not be highpass-filtered"):
         plot_bridged_electrodes(raw_)
     raw_ = raw.copy().filter(None, 10)
     with pytest.raises(RuntimeError, match="should not be lowpass-filtered"):
         plot_bridged_electrodes(raw_)
+
+    plt.close("all")
