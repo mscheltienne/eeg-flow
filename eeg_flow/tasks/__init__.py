ff3a1a96ae19d4a89b37f160dd6606a7151e2161
Arthur Nguyen <arthur.nguyen@live.com>
2023-05-24 01:38:07 +0200

    fix import of compare_labels

--- eeg_flow/tasks/__init__.py
+++ eeg_flow/tasks/__init__.py
@@ -4,4 +4,4 @@ from .annotate_bad_channels_and_segments import (  # noqa: F401
     annotate_bad_channels_and_segments,
 )
 from .convert_xdf_to_fiff import convert_xdf_to_fiff  # noqa: F401
-from .ica_decomposition import fit_icas, label_components  # noqa: F401
+from .ica_decomposition import compare_labels, fit_icas, label_components  # noqa: F401
