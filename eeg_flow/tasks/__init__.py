88b61c549234703e9606f0eeec429a15634390db
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-06-20 10:39:14 +0200

    update notebook

--- eeg_flow/tasks/__init__.py
+++ eeg_flow/tasks/__init__.py
@@ -2,6 +2,7 @@
 
 from .annotate_bad_channels_and_segments import (  # noqa: F401
     annotate_bad_channels_and_segments,
+    view_annotated_raw,
 )
 from .convert_xdf_to_fiff import convert_xdf_to_fiff  # noqa: F401
 from .ica_decomposition import (  # noqa: F401
