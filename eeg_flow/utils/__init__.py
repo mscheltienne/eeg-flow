53419cedd292ebc21253ca7f0cb86f5d3ad7eb83
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-02-06 15:12:39 +0100

    better imports

--- eeg_flow/utils/__init__.py
+++ eeg_flow/utils/__init__.py
@@ -1,3 +1,8 @@
 """Utilities module."""
 
+from . import annotations  # noqa: F401
+from . import bids  # noqa: F401
+from . import concurrency  # noqa: F401
+from . import config  # noqa: F401
+from . import logs  # noqa: F401
 from .parallel import parallel  # noqa: F401
