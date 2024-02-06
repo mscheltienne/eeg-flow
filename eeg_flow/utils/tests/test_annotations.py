c80d14e83b507f4c2b5b8b676ace90c7a14c9fbf
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-02-06 15:46:13 +0100

    better pytest config

--- eeg_flow/utils/tests/test_annotations.py
+++ eeg_flow/utils/tests/test_annotations.py
@@ -1,26 +1,43 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import numpy as np
+import pytest
 from mne import create_info
 from mne.io import RawArray
 
 from eeg_flow.config import load_triggers
 from eeg_flow.utils.annotations import annotations_from_events
 
-# create fake data
-rng = np.random.default_rng(101)
-fs = 100  # Hz
-data = np.zeros(fs * 10)
-onsets = dict()
-triggers = load_triggers()
-for k, (key, value) in enumerate(triggers.items()):
-    idx = rng.integers(low=0, high=int(data.size - 0.2 * fs), size=k + 1)
-    data[idx] = value
-    onsets[key] = idx / fs
-info = create_info(["TRIGGER"], fs, "stim")
-raw = RawArray(data.reshape(1, -1), info)
+if TYPE_CHECKING:
+    from mne.io import BaseRaw
 
 
-def test_annotations_from_events():
+@pytest.fixture(scope="module")
+def triggers():
+    """Load triggers."""
+    return load_triggers()
+
+
+@pytest.fixture(scope="module")
+def data(triggers: dict[str, int]) -> tuple[BaseRaw, dict[str, float]]:
+    """Create fake raw object."""
+    rng = np.random.default_rng(101)
+    fs = 100  # Hz
+    data = np.zeros(fs * 10)
+    onsets = dict()
+    for k, (key, value) in enumerate(triggers.items()):
+        idx = rng.integers(low=0, high=int(data.size - 0.2 * fs), size=k + 1)
+        data[idx] = value
+        onsets[key] = idx / fs
+    info = create_info(["TRIGGER"], fs, "stim")
+    return RawArray(data.reshape(1, -1), info), onsets
+
+
+def test_annotations_from_events(data, triggers):
     """Test annotations_from_events."""
+    raw, onsets = data  # unpack
     annots = annotations_from_events(raw)
     assert len(triggers) * (len(triggers) + 1) / 2 == len(annots)
     assert sorted(np.unique(annots.description)) == sorted(triggers.keys())
